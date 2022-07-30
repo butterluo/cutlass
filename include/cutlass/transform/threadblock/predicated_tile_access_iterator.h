7f33851f929ab73537e3cdd34bc6c8b0f324d301
butterluo <luo_1cn@163.com>
2022-07-30 15:21:22 +0800

    tt

--- include/cutlass/transform/threadblock/predicated_tile_access_iterator.h
+++ include/cutlass/transform/threadblock/predicated_tile_access_iterator.h
@@ -473,7 +473,7 @@ class PredicatedTileAccessIterator<Shape_, Element_, layout::PitchLinear,
       if (kAdvanceRank) {
         pointer_ += params_.inc_advance_ * LongIndex(tile_offset.strided() - 1);
         pointer_ += Shape::kContiguous * tile_offset.contiguous();
-      } else {
+      } else {//BTBT TensorCoord tile_offset{1,0} if kAdvanceRank=0 else {0,1}
         pointer_ += params_.inc_advance_ * LongIndex(tile_offset.contiguous() - 1);
         pointer_ += Shape::kStrided * tile_offset.strided();
       }
@@ -1139,7 +1139,7 @@ class PredicatedTileAccessIterator<Shape_, Element_, layout::AffineRankN<2>,
       if (kAdvanceRank) {
         pointer_ += params_.inc_advance_ * LongIndex(tile_offset[1] - 1);
         pointer_ += Shape::kContiguous * tile_offset[0];
-      } else {// TensorCoord tile_offset{1,0} if kAdvanceRank=0 else {0,1}
+      } else {
         pointer_ += params_.inc_advance_ * LongIndex(tile_offset[0] - 1);
         pointer_ += Shape::kStrided * tile_offset[1];
       }
